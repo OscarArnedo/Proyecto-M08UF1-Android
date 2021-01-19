bb7a8bfaf988493e98e0cd1c24351a6e44d200ec
OscarArnedo <oscar.arnedo.riccobene@gmail.com>
2021-01-19 17:01:13 +0100

    Segundo commit, ya está implementada la lógica de las letras pulsadas.

--- app/src/main/java/com/example/arnedo_perezmedrano_practica_android_m08uf1/GameActivity.java
+++ app/src/main/java/com/example/arnedo_perezmedrano_practica_android_m08uf1/GameActivity.java
@@ -3,19 +3,24 @@ package com.example.arnedo_perezmedrano_practica_android_m08uf1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class GameActivity extends AppCompatActivity {
 
     private static String[] palabras = {"MANZANA","PIMIENTO", "COLIFLOR", "BERENJENA", "PATATA", "CALABAZA", "ACELGAS", "TOMATE"};
-    private TextView palabra;
+    private TextView tvPalabra;
     private static int[] imagenes = {R.drawable.ahorcado0,R.drawable.ahorcado1,R.drawable.ahorcado2,R.drawable.ahorcado3,R.drawable.ahorcado4,R.drawable.ahorcado5,R.drawable.ahorcado6,R.drawable.ahorcado7,R.drawable.ahorcado8,R.drawable.ahorcado9,R.drawable.ahorcado10, };
     private ImageView imagen;
+    List<String> arrayCompletar = new ArrayList<String>();
+    private String palabraSeleccionada;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,10 +28,11 @@ public class GameActivity extends AppCompatActivity {
         setContentView(R.layout.activity_game);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        palabra = (TextView) findViewById(R.id.palabra);
+        tvPalabra = (TextView) findViewById(R.id.palabra);
         Random rand = new Random();
         int random = rand.nextInt(palabras.length);
-        palabra.setText(hideWord(palabras[random]));
+        palabraSeleccionada = palabras[random];
+        tvPalabra.setText(hideWord(palabraSeleccionada));
 
         imagen = (ImageView) findViewById(R.id.imagen);
     }
@@ -35,6 +41,7 @@ public class GameActivity extends AppCompatActivity {
         String guiones = "";
         for (int i = 0; i < word.length(); i++) {
             guiones += "_ ";
+            arrayCompletar.add("_");
         }
 
         return guiones;
@@ -42,7 +49,33 @@ public class GameActivity extends AppCompatActivity {
 
     public void letraPulsada(View v) {
         Button b = (Button) v;
-        palabra.setText(b.getText());
+        //palabra.setText(b.getText());
+        String stLetraPulsada = String.valueOf(b.getText());
+        String p = (String) tvPalabra.getText();
+        b.setEnabled(false);
 
+        if (palabraSeleccionada.contains(stLetraPulsada)){
+            for(int i = 0; i < palabraSeleccionada.length(); i++){
+                char c = palabraSeleccionada.charAt(i);
+                if (stLetraPulsada.charAt(0) == c) {
+                    //cambiarLetra(stLetraPulsada, i);
+                    arrayCompletar.set(i, stLetraPulsada);
+                }
+
+                String textoAMostrar = "";
+                String palabraComprobante = "";
+
+                for (int x = 0; x < arrayCompletar.size(); x++) {
+                    textoAMostrar += " " + arrayCompletar.get(x);
+                    palabraComprobante +=  arrayCompletar.get(x);
+                }
+                tvPalabra.setText(textoAMostrar);
+            }
+        }
     }
+
+    /*public void cambiarLetra(String stLetraPulsada, int posicion){
+        arrayCompletar.set(posicion, String.valueOf(stLetraPulsada));
+
+    }*/
 }
